f3eb828d2059e889a7db5f1994fca65d3f1409da
Shreekesh <yadavshreekesh760@gmail.com>
2025-09-04 10:54:35 +0530

    Step2: Setup common-lib with DTOs, exception, and utils

---
+++ src/main/java/com/banking/common/util/Constants.java
@@ -0,0 +1,7 @@
+package com.banking.common.util;
+
+public class Constants {
+    public static final String SERVICE_UNAVAILABLE = "Service is temporarily unavailable.";
+    public static final String SUCCESS = "Success";
+    public static final String FAILURE = "Failure";
+}
